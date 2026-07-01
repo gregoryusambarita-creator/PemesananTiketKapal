10be8771b9c62cd08d149a7d3313a45edb9958a3
Gregoryus Ambarita <gregoryusambarita@gmail.com>
2026-07-01 17:47:16 +0700

    Perbaikan Masalah pada route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,13 @@ use App\Http\Controllers\JadwalController;
 use App\Http\Controllers\PemesananController;
 use App\Http\Controllers\PembayaranController;
 use App\Http\Controllers\LaporanController;
+use App\Http\Controllers\UserController;
+
+use App\Models\Kapal;
+use App\Models\Rute;
+use App\Models\Jadwal;
+use App\Models\Pemesanan;
+use App\Models\Pembayaran;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +22,9 @@ use App\Http\Controllers\LaporanController;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/login', [AuthController::class, 'loginForm'])
-    ->name('login');
-
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 /*
 |--------------------------------------------------------------------------
@@ -36,23 +39,32 @@ Route::middleware('auth')->group(function () {
     | Dashboard
     |--------------------------------------------------------------------------
     */
-
     Route::get('/', function () {
-        return view('dashboard.index');
-    });
+
+        $totalKapal = Kapal::count();
+        $totalRute = Rute::count();
+        $totalJadwal = Jadwal::count();
+        $totalPemesanan = Pemesanan::count();
+        $totalPendapatan = Pembayaran::sum('jumlah_bayar');
+
+        return view('dashboard.index', compact(
+            'totalKapal',
+            'totalRute',
+            'totalJadwal',
+            'totalPemesanan',
+            'totalPendapatan'
+        ));
+    })->name('dashboard');
 
     /*
     |--------------------------------------------------------------------------
     | Khusus Admin
     |--------------------------------------------------------------------------
     */
-
     Route::middleware('role:admin')->group(function () {
-
         Route::resource('kapal', KapalController::class);
-
         Route::resource('rute', RuteController::class);
-
+        Route::resource('users', UserController::class);
     });
 
     /*
@@ -60,14 +72,10 @@ Route::middleware('auth')->group(function () {
     | Admin dan Petugas
     |--------------------------------------------------------------------------
     */
-
     Route::resource('jadwal', JadwalController::class);
-
     Route::resource('pemesanan', PemesananController::class);
-
     Route::resource('pembayaran', PembayaranController::class);
 
     Route::get('/laporan', [LaporanController::class, 'index'])
         ->name('laporan.index');
-
 });
